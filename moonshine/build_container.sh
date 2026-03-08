b6941a02999f1b0382287913c96119ed0a6ed78b
Holden Karau <holden@pigscanfly.ca>
2026-03-08 00:40:37 -0800

    I think slim and rcX.d might not be a party

--- moonshine/build_container.sh
+++ moonshine/build_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MYORG=${MYORG:-holdenk}
-VERSION=${VERSION:-0.0.1}
+VERSION=${VERSION:-0.0.2}
 BUILDX_CMD=${BUILDX_CMD:-push}
 PLATFORM=${PLATFORM:-linux/amd64,linux/arm64}
 image="${MYORG}/moonshine4homeassistant:${VERSION}"
