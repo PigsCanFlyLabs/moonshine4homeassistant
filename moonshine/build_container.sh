601c98d502020baad9e4672e299654731b085fde
Holden Karau <holden@pigscanfly.ca>
2026-03-29 15:35:52 -0700

    Bump version and try to simplify build

--- moonshine/build_container.sh
+++ moonshine/build_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MYORG=${MYORG:-holdenk}
-VERSION=${VERSION:-0.0.14e}
+VERSION=${VERSION:-0.0.15}
 BUILDX_CMD=${BUILDX_CMD:-push}
 PLATFORM=${PLATFORM:-linux/amd64,linux/arm64}
 image="${MYORG}/moonshine4homeassistant:${VERSION}"
-docker buildx build --platform="${PLATFORM}" -t "${image}" -f "${dockerfile}" "--${BUILDX_CMD}" .
+docker buildx build --platform="${PLATFORM}" -t "${image}" -f "${dockerfile}" "--${BUILDX_CMD}" --build-arg VERSION=${VERSION} .
