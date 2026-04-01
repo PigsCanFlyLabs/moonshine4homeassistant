6b66f23e7d8e3919f0d3e166b514a430323b1dda
Holden Karau <holden@pigscanfly.ca>
2026-03-31 22:47:51 -0700

    Log some bytes.

--- moonshine/build_container.sh
+++ moonshine/build_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 MYORG=${MYORG:-holdenk}
-VERSION=${VERSION:-0.0.23}
+VERSION=${VERSION:-0.0.24}
 BUILDX_CMD=${BUILDX_CMD:-push}
 PLATFORM=${PLATFORM:-linux/amd64,linux/arm64}
 image="${MYORG}/moonshine4homeassistant:${VERSION}"
