8457fcdd7cd44a7f6e5bbcffe32062380843bd95
Holden Karau <holden@pigscanfly.ca>
2026-03-31 22:44:56 -0700

    Try without convert.

--- moonshine/build_container.sh
+++ moonshine/build_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 MYORG=${MYORG:-holdenk}
-VERSION=${VERSION:-0.0.22}
+VERSION=${VERSION:-0.0.23}
 BUILDX_CMD=${BUILDX_CMD:-push}
 PLATFORM=${PLATFORM:-linux/amd64,linux/arm64}
 image="${MYORG}/moonshine4homeassistant:${VERSION}"
